ce528901afb8545417a9150cc579621b22846b17
Zhiyuan Guo <k22036998@kcl.ac.uk>
2023-10-04 15:02:03 +0100

    tetsayuiyd

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(models.Model):
     name = models.TextField(default='', unique=True, blank=False, max_length=29)
     description = models.TextField(default='', max_length=119, blank=True)
-    quantity = models.IntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    quantity = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     # def __init__(self, name, description, quantity):
     #     self.name = name
