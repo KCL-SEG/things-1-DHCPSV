bad629469abfeedf44e3958c21559312f5844e88
Zhiyuan Guo <k22036998@kcl.ac.uk>
2023-10-04 14:28:33 +0100

    changed

--- things/models.py
+++ things/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 class Thing(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    quantity = models.IntegerField()
 
-    name = models.TextField
-    description = models.TextField
-    quantity = models.IntegerField
-
-    def __init__(self, name, description, quantity):
-        self.name = name
-        self.description = description
-        self.quantity = quantity
+    # def __init__(self, name, description, quantity):
+    #     self.name = name
+    #     self.description = description
+    #     self.quantity = quantity
