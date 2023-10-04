acc8a9472be13a2e7e0db6ee83084a9a10764572
Zhiyuan Guo <k22036998@kcl.ac.uk>
2023-10-04 13:53:43 +0100

    improve models.py

--- things/models.py
+++ things/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 class Thing():
+    
     name = models.TextField
     description = models.TextField
     quantity = models.IntegerField
+
+    def __init__(name, description, quantity):
+        name = name
+        description = description
+        quantity = quantity
