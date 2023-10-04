aa9456f672c133ba02d73f216aa2962414177a67
Zhiyuan Guo <k22036998@kcl.ac.uk>
2023-10-04 14:32:46 +0100

    testfinal

--- things/models.py
+++ things/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.TextField(default='')
+    description = models.TextField(default='')
+    quantity = models.IntegerField(default=0)
 
     # def __init__(self, name, description, quantity):
     #     self.name = name
