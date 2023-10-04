3a75a08c59715b0f41be9c3f76bcd9ac79ade36c
Zhiyuan Guo <k22036998@kcl.ac.uk>
2023-10-04 13:56:31 +0100

    modv2

--- things/models.py
+++ things/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Thing():
-    
+
     name = models.TextField
     description = models.TextField
     quantity = models.IntegerField
 
-    def __init__(name, description, quantity):
-        name = name
-        description = description
-        quantity = quantity
+    def __init__(thing_name, thing_description, thing_quantity):
+        name = thing_name
+        description = thing_description
+        quantity = thing_quantity
